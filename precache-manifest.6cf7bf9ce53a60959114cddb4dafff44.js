72ea9672a05d0b2c75b522ba6b7d6f5d33b1b2c4
NicolasM99 <moreno.pnicolas@gmail.com>
2020-09-09 19:32:23 -0500

    Updates

---
+++ precache-manifest.6cf7bf9ce53a60959114cddb4dafff44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a3745f634b35ec02b0c3f0c3755335",
+    "url": "/portafolio_multimedia.git/index.html"
+  },
+  {
+    "revision": "1c5cb02afd6f668ee2b3",
+    "url": "/portafolio_multimedia.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10d8cf91f40bfd62d389",
+    "url": "/portafolio_multimedia.git/static/js/2.7db170b3.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/portafolio_multimedia.git/static/js/2.7db170b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5cb02afd6f668ee2b3",
+    "url": "/portafolio_multimedia.git/static/js/main.db14d7cf.chunk.js"
+  },
+  {
+    "revision": "78f810916ae892557e77",
+    "url": "/portafolio_multimedia.git/static/js/runtime-main.3869e0b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio_multimedia.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
