2d245e0a1956506c9ee05c68aee433dde61c871d
Nicolas Moreno Posada <moreno.pnicolas@gmail.com>
2020-09-28 13:33:02 -0500

    aumente el tamaño de la imagen de perfil para que no se viera tan pixelada

--- src/components/pages/PrimerRegistro.js
+++ src/components/pages/PrimerRegistro.js
@@ -48,8 +48,8 @@ const PrimerRegistro = (props) => {
     new Promise((resolve) => {
       Resizer.imageFileResizer(
         file,
-        300,
-        300,
+        600,
+        600,
         "JPEG",
         100,
         0,
