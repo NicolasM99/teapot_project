b6e2047408144c15e51252aba8b71894a121e1d3
NicolasM99 <moreno.pnicolas@gmail.com>
2020-09-29 20:33:50 -0500

    agregué la ventana de términos y condiciones

--- src/components/pages/PrimerRegistro.js
+++ src/components/pages/PrimerRegistro.js
@@ -5,7 +5,7 @@ import { firestore, storage } from "../functions/Firebase.js";
 import "../styles/text-input-style.css";
 import "../styles/loader.css";
 import { useHistory } from "react-router-dom";
-import MyNavbar2 from "../MyNavbar2";
+import TerminosCondiciones from "./TerminosCondiciones.js";
 import {
   Modal,
   Button,
@@ -19,14 +19,15 @@ const PrimerRegistro = (props) => {
   const [receivedData, setReceivedData] = useState(false);
   const db = firestore;
   const [data, setData] = useState("");
-  const [send, setSend] = useState(false);
+  //const [send, setSend] = useState(false);
   const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null); //AGREGAR EL PREVIEW DE LA IMAGEN QUE SE CARGA
   const [progress, setProgress] = useState(null);
   const [sending, setSending] = useState(false);
   const [sent, setSent] = useState(false);
-  const [imageURL, setImageURL] = useState("");
+  //const [imageURL, setImageURL] = useState("");
   const isMountedRef = useRef(null);
+  const [showTerms, setShowTerms] = useState(null);
 
   useEffect(() => {
     if (receivedData) {
@@ -282,6 +283,7 @@ const PrimerRegistro = (props) => {
     setShowCertificates(!showCertificates);
   }
   const handleClose = () => setShowModal(false);
+  const handleCloseTerms = () => setShowTerms(false);
 
   if (sending) {
     console.log("SENDING...");
@@ -308,6 +310,230 @@ const PrimerRegistro = (props) => {
   } else {
     return (
       <>
+        <Modal
+          show={showTerms}
+          onHide={handleCloseTerms}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Términos y condiciones
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum. Sed
+              ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur? But I must explain to you how all this
+              mistaken idea of denouncing pleasure and praising pain was born
+              and I will give you a complete account of the system, and expound
+              the actual teachings of the great explorer of the truth, the
+              master-builder of human happiness. No one rejects, dislikes, or
+              avoids pleasure itself, because it is pleasure, but because those
+              who do not know how to pursue pleasure rationally encounter
+              consequences that are extremely painful. Nor again is there anyone
+              who loves or pursues or desires to obtain pain of itself, because
+              it is pain, but because occasionally circumstances occur in which
+              toil and pain can procure him some great pleasure. To take a
+              trivial example, which of us ever undertakes laborious physical
+              exercise, except to obtain some advantage from it? But who has any
+              right to find fault with a man who chooses to enjoy a pleasure
+              that has no annoying consequences, or one who avoids a pain that
+              produces no resultant pleasure? At vero eos et accusamus et iusto
+              odio dignissimos ducimus qui blanditiis praesentium voluptatum
+              deleniti atque corrupti quos dolores et quas molestias excepturi
+              sint occaecati cupiditate non provident, similique sunt in culpa
+              qui officia deserunt mollitia animi, id est laborum et dolorum
+              fuga. Et harum quidem rerum facilis est et expedita distinctio.
+              Nam libero tempore, cum soluta nobis est eligendi optio cumque
+              nihil impedit quo minus id quod maxime placeat facere possimus,
+              omnis voluptas assumenda est, omnis dolor repellendus. Temporibus
+              autem quibusdam et aut officiis debitis aut rerum necessitatibus
+              saepe eveniet ut et voluptates repudiandae sint et molestiae non
+              recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut
+              aut reiciendis voluptatibus maiores alias consequatur aut
+              perferendis doloribus asperiores repellat. Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+              unde omnis iste natus error sit voluptatem accusantium doloremque
+              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+              veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+              ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia
+              non numquam eius modi tempora incidunt ut labore et dolore magnam
+              aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
+              ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
+              qui in ea voluptate velit esse quam nihil molestiae consequatur,
+              vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? But
+              I must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the system, and expound the actual teachings of the
+              great explorer of the truth, the master-builder of human
+              happiness. No one rejects, dislikes, or avoids pleasure itself,
+              because it is pleasure, but because those who do not know how to
+              pursue pleasure rationally encounter consequences that are
+              extremely painful. Nor again is there anyone who loves or pursues
+              or desires to obtain pain of itself, because it is pain, but
+              because occasionally circumstances occur in which toil and pain
+              can procure him some great pleasure. To take a trivial example,
+              which of us ever undertakes laborious physical exercise, except to
+              obtain some advantage from it? But who has any right to find fault
+              with a man who chooses to enjoy a pleasure that has no annoying
+              consequences, or one who avoids a pain that produces no resultant
+              pleasure? At vero eos et accusamus et iusto odio dignissimos
+              ducimus qui blanditiis praesentium voluptatum deleniti atque
+              corrupti quos dolores et quas molestias excepturi sint occaecati
+              cupiditate non provident, similique sunt in culpa qui officia
+              deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
+              quidem rerum facilis est et expedita distinctio. Nam libero
+              tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+              quo minus id quod maxime placeat facere possimus, omnis voluptas
+              assumenda est, omnis dolor repellendus. Temporibus autem quibusdam
+              et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
+              et voluptates repudiandae sint et molestiae non recusandae. Itaque
+              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptatibus maiores alias consequatur aut perferendis doloribus
+              asperiores repellat.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum. Sed
+              ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur? But I must explain to you how all this
+              mistaken idea of denouncing pleasure and praising pain was born
+              and I will give you a complete account of the system, and expound
+              the actual teachings of the great explorer of the truth, the
+              master-builder of human happiness. No one rejects, dislikes, or
+              avoids pleasure itself, because it is pleasure, but because those
+              who do not know how to pursue pleasure rationally encounter
+              consequences that are extremely painful. Nor again is there anyone
+              who loves or pursues or desires to obtain pain of itself, because
+              it is pain, but because occasionally circumstances occur in which
+              toil and pain can procure him some great pleasure. To take a
+              trivial example, which of us ever undertakes laborious physical
+              exercise, except to obtain some advantage from it? But who has any
+              right to find fault with a man who chooses to enjoy a pleasure
+              that has no annoying consequences, or one who avoids a pain that
+              produces no resultant pleasure? At vero eos et accusamus et iusto
+              odio dignissimos ducimus qui blanditiis praesentium voluptatum
+              deleniti atque corrupti quos dolores et quas molestias excepturi
+              sint occaecati cupiditate non provident, similique sunt in culpa
+              qui officia deserunt mollitia animi, id est laborum et dolorum
+              fuga. Et harum quidem rerum facilis est et expedita distinctio.
+              Nam libero tempore, cum soluta nobis est eligendi optio cumque
+              nihil impedit quo minus id quod maxime placeat facere possimus,
+              omnis voluptas assumenda est, omnis dolor repellendus. Temporibus
+              autem quibusdam et aut officiis debitis aut rerum necessitatibus
+              saepe eveniet ut et voluptates repudiandae sint et molestiae non
+              recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut
+              aut reiciendis voluptatibus maiores alias consequatur aut
+              perferendis doloribus asperiores repellat. Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+              unde omnis iste natus error sit voluptatem accusantium doloremque
+              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+              veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+              ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia
+              non numquam eius modi tempora incidunt ut labore et dolore magnam
+              aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
+              ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
+              qui in ea voluptate velit esse quam nihil molestiae consequatur,
+              vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? But
+              I must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the system, and expound the actual teachings of the
+              great explorer of the truth, the master-builder of human
+              happiness. No one rejects, dislikes, or avoids pleasure itself,
+              because it is pleasure, but because those who do not know how to
+              pursue pleasure rationally encounter consequences that are
+              extremely painful. Nor again is there anyone who loves or pursues
+              or desires to obtain pain of itself, because it is pain, but
+              because occasionally circumstances occur in which toil and pain
+              can procure him some great pleasure. To take a trivial example,
+              which of us ever undertakes laborious physical exercise, except to
+              obtain some advantage from it? But who has any right to find fault
+              with a man who chooses to enjoy a pleasure that has no annoying
+              consequences, or one who avoids a pain that produces no resultant
+              pleasure? At vero eos et accusamus et iusto odio dignissimos
+              ducimus qui blanditiis praesentium voluptatum deleniti atque
+              corrupti quos dolores et quas molestias excepturi sint occaecati
+              cupiditate non provident, similique sunt in culpa qui officia
+              deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
+              quidem rerum facilis est et expedita distinctio. Nam libero
+              tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+              quo minus id quod maxime placeat facere possimus, omnis voluptas
+              assumenda est, omnis dolor repellendus. Temporibus autem quibusdam
+              et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
+              et voluptates repudiandae sint et molestiae non recusandae. Itaque
+              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptatibus maiores alias consequatur aut perferendis doloribus
+              asperiores repellat.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              style={{ textTransform: "none" }}
+              variant="danger"
+              onClick={() => handleCloseTerms()}
+            >
+              <b>Cerrar</b>
+            </Button>
+          </Modal.Footer>
+        </Modal>
         <Modal
           show={showModal}
           onHide={handleClose}
@@ -719,7 +945,11 @@ const PrimerRegistro = (props) => {
                   onChange={() => setEnableContinue(!enableContinue)}
                 />
                 <label className="custom-control-label" htmlFor="termsCheckbox">
-                  Acepto los <a href="#"> términos y condiciones.</a>
+                  Acepto los{" "}
+                  <a href="#" onClick={() => setShowTerms(true)}>
+                    {" "}
+                    términos y condiciones.
+                  </a>
                 </label>
               </div>
               {enableContinue ? (
