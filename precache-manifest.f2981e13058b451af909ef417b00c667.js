78ce30455d55b95b00604b98af3ee495f435b74d
NicolasM99 <moreno.pnicolas@gmail.com>
2020-10-13 19:48:20 -0500

    Updates

---
+++ precache-manifest.f2981e13058b451af909ef417b00c667.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe7a5802d042f53f469f59c30552939",
+    "url": "/portafolio_multimedia/index.html"
+  },
+  {
+    "revision": "bd1c9728283da5eff476",
+    "url": "/portafolio_multimedia/static/css/2.edeedb01.chunk.css"
+  },
+  {
+    "revision": "d8217a7ea6e307f030ed",
+    "url": "/portafolio_multimedia/static/css/main.13490f83.chunk.css"
+  },
+  {
+    "revision": "bd1c9728283da5eff476",
+    "url": "/portafolio_multimedia/static/js/2.9997c5e7.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/portafolio_multimedia/static/js/2.9997c5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8217a7ea6e307f030ed",
+    "url": "/portafolio_multimedia/static/js/main.b0bcb4b9.chunk.js"
+  },
+  {
+    "revision": "061d8228061202212c9e",
+    "url": "/portafolio_multimedia/static/js/runtime-main.5deda5af.js"
+  },
+  {
+    "revision": "57bc5bfea37d0376afe2b4fd34e5bc50",
+    "url": "/portafolio_multimedia/static/media/3d.57bc5bfe.png"
+  },
+  {
+    "revision": "07d05f9c9b8f5e1aff72af5736416e4a",
+    "url": "/portafolio_multimedia/static/media/desarrollo.07d05f9c.jpg"
+  },
+  {
+    "revision": "dfeccb36079f0376a27c0a3e7e8e1b1f",
+    "url": "/portafolio_multimedia/static/media/diseño.dfeccb36.jpg"
+  },
+  {
+    "revision": "ed48b1b34ce274dee0b417437a8e9c69",
+    "url": "/portafolio_multimedia/static/media/logo_multifolio_sin_fondo.ed48b1b3.png"
+  },
+  {
+    "revision": "d31dde3a0433e5033898b018e5f1b641",
+    "url": "/portafolio_multimedia/static/media/produccion.d31dde3a.jpg"
+  }
+]);
